abd308e69c0c423f5f3d88f6740b0935359dec76
Adam Gutierrez <adam.gutierrez@hyland.com>
2021-01-20 15:25:46 -0600

    Add project files.

---
+++ StopWatch2/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+
+namespace StopWatch2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stopwatch sw = new Stopwatch();
+            sw.Start();
+            System.Threading.Thread.Sleep(1000);
+            sw.getTime();
+            System.Threading.Thread.Sleep(2000);
+            sw.Stop();
+
+            sw.getTime();
+        }
+    }
+}
---
+++ StopWatch2/Stopwatch.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+
+namespace StopWatch2
+{
+    class Stopwatch
+    {
+        TimeSpan duration = TimeSpan.Zero;
+        DateTime startTime;
+        DateTime stopTime;
+        bool isRunning;
+
+        public void Start()
+        {
+            if (isRunning == false)
+            {
+                startTime = DateTime.Now;
+                duration = TimeSpan.Zero;
+                isRunning = true;
+            } else
+            {
+                throw new InvalidOperationException("The stopwatch is already running.");
+            }
+
+        }
+        public void Stop()
+        {
+            if (isRunning == true)
+            {
+                stopTime = DateTime.Now;
+                duration = stopTime - startTime;
+            } else
+            {
+                throw new InvalidOperationException("The stopwatch is not running.");
+            }
+        }
+
+        public void getTime()
+        {
+            if (isRunning == true)
+            {
+                Console.WriteLine("Stopwatch time is: " + (DateTime.Now - startTime));
+            } else
+            {
+                Console.WriteLine(duration);
+            }
+        }
+
+
+    }
+}
